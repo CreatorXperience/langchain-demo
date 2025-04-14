030fa1494ce5eb8ec209fbfad78e2f6b6c11cdac
Muhammad Habeeb <allyearmustobey@gmail.com>
2025-04-14 15:49:45 +0100

    complete langchain chatbot

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,14 +1,20 @@
-from langchain_core.messages import HumanMessage, BaseMessage
+from langchain_core.messages import (
+    HumanMessage,
+    BaseMessage,
+    trim_messages,
+    SystemMessage,
+    AIMessage,
+)
 from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import InMemorySaver
-from langgraph.graph import StateGraph, MessagesState, START
+from langgraph.graph import StateGraph, START
 from langgraph.graph.message import add_messages
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 import dotenv
 from getpass import getpass
 from os import environ, path
 import asyncio
-from typing import TypedDict, Annotated, Sequence, List
+from typing import TypedDict, Annotated, Sequence
 
 
 class State(TypedDict):
@@ -19,6 +25,7 @@ class State(TypedDict):
 env_path = path.expanduser("~") + "/langchain-python/.env"
 dotenv.load_dotenv(env_path, encoding="utf-8")
 model = init_chat_model("llama3-8b-8192", model_provider="groq")
+
 
 key = dotenv.get_key(env_path, "GROQ_API_KEY")
 if not key:
@@ -40,10 +47,20 @@ prompt_template = ChatPromptTemplate(
 )
 
 
-async def call_model(state: State):
-    prompt = prompt_template.invoke(state)
-    response = await model.ainvoke(prompt)
-    response.pretty_print()
+def call_model(state: State):
+    trimmed_msgs = trim_messages(
+        messages=state["messages"],
+        token_counter=model,
+        max_tokens=65,
+        strategy="last",
+        start_on="human",
+        include_system=True,
+    )
+    prompt = prompt_template.invoke(
+        {"messages": trimmed_msgs, "language": state["language"]}
+    )
+    response = model.invoke(prompt)
+    print(response)
     return {"messages": [response]}
 
 
@@ -54,14 +71,42 @@ app = workflow.compile(checkpointer=memory)
 
 config = {"configurable": {"thread_id": "12345678"}}
 
+messages = [
+    SystemMessage(content="you're a good assistant"),
+    HumanMessage(content="hi! I'm bob"),
+    AIMessage(content="hi!"),
+    HumanMessage(content="I like vanilla ice cream"),
+    AIMessage(content="nice"),
+    HumanMessage(content="whats 2 + 2"),
+    AIMessage(content="4"),
+    HumanMessage(content="thanks"),
+    AIMessage(content="no problem!"),
+    HumanMessage(content="having fun?"),
+    AIMessage(content="yes!"),
+]
 
-prompt = "What's is binary?"
 
-msgs = [HumanMessage(prompt)]
+prompt = "Hi, I'm Bob?"
+
+msgs = messages
 
 
 async def run():
-    res = await app.ainvoke({"messages": msgs, "language": "spanish"}, config)
+    global prompt, msgs
+
+    for chunk, _ in app.stream(
+        {"messages": msgs, "language": "English"}, config, stream_mode="messages"
+    ):
+        if isinstance(chunk, AIMessage):
+            print(chunk.content, end=" ")
+
+    prompt = "What's my name"
+    msgs = messages + [HumanMessage(prompt)]
+    # await second_run()
+
+
+async def second_run():
+    res = await app.ainvoke({"messages": msgs, "language": "English"}, config)
     res["messages"][-1].pretty_print()
 
 
